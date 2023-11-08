ec74d26bb7201277b80d253aa5f8dbb4ca2078d5
John-Bugler <ijt.trubac@gmail.com>
2023-11-08 18:47:00 +0100

    Update Stocks_data.py

--- Stocks_data.py
+++ Stocks_data.py
@@ -7,8 +7,35 @@ connection = pyodbc.connect(f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={s
 
 
 cursor = connection.cursor()
-cursor.execute('select * from revolut_stocks order by date asc')
-for row in cursor:
-    print(row)
+cursor.execute("select ticker from [reports].[dbo].[revolut_stocks] where type = 'BUY - MARKET' group by ticker order by ticker asc;")
+
+portfolio_tickers = cursor.fetchall()
+print(portfolio_tickers)
+
+portfolio_tickers_cleaned = [item[0] for item in portfolio_tickers]
+print(portfolio_tickers_cleaned)
+
+#for row in portfolio_tickers:
+#    print(row)
+
+
+import yfinance as yf
+import pandas as pd
+
+    
+df_list = list()
+for ticker in portfolio_tickers_cleaned:
+    data = yf.download(ticker, group_by="Ticker", period='1d')
+    data['ticker'] = ticker           # add this column because the dataframe doesn't contain a column with the ticker
+    df_list.append(data)
+
+# combine all dataframes into a single dataframe
+df = pd.concat(df_list)
+print(df)
+
+
+
+
+
 
 connection.close()
